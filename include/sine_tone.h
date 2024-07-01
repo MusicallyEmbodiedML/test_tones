a8a858f2bfb55a87360e12802edde22031b2d353
Andrea Martelloni <a.martelloni@sussex.ac.uk>
2024-07-01 15:02:01 +0100

    Added toy test sine tone generator

---
+++ include/sine_tone.h
@@ -0,0 +1,10 @@
+#ifndef _SINE_TONE_H_
+#define _SINE_TONE_H_
+
+
+extern void sine_tone_init(float sample_rate, float freq);
+
+extern int32_t sine_tone_generate(void);
+
+
+#endif  // _SINE_TONE_H_
